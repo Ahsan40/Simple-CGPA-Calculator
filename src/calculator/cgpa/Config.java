ab72212537c212170d508b079d445aab898cc1a1
Ahsan Khan <nobody.ahsan@gmail.com>
2021-10-30 20:45:22 +0600

    added Configs

---
+++ src/calculator/cgpa/Config.java
@@ -0,0 +1,7 @@
+package calculator.cgpa;
+
+public class Config {
+    public static final String title = "Simple CGPA Calculator";
+    public static final String version = "v1.0";
+    public static final String fileName = "resultData.txt";
+}
